03ccb88e96fb51c54ba2d9911dcb5407330fe641
michaelferrell <ferrell.michaelw@gmail.com>
2023-03-25 20:01:23 -0400

    lots of progress on precise backtest

--- ml_models/forecast_top_stocks_model.py
+++ ml_models/forecast_top_stocks_model.py
@@ -143,7 +143,7 @@ def list_of_filing_weeks_for_training(keyword, filing_type, stock_symbol, interv
         datetime_list.append(timestamp.strftime("%Y-%m-%d"))
     return datetime_list
 
-
+#this is the model training used all of the charts & tables up to 3/24/23
 def train_ml_model(keyword, filing_type, stock_symbol, interval, dates):
     training_dataset = f'''
                      with matched_dates as (
@@ -252,6 +252,117 @@ def train_ml_model(keyword, filing_type, stock_symbol, interval, dates):
     return df_test_full, df_test, mae, model
 
 
+#the function above uses all historical data to train the model on the next week's prediction. This function will only
+# use the data points within the period we observe strong correlation, and then try to predict the next week
+#this will be used for the precise backtest
+def train_narrow_ml_model(keyword, filing_type, stock_symbol, interval, end_date, correlation_start_date):
+    training_dataset = f'''
+                     with matched_dates as (
+                     with rolling_average_calculation as (
+                       with keyword_data as (select * from keyword_weekly_counts where keyword = '{keyword}' and filing_type = '{filing_type}'),
+                     stock_weekly_opening as (select * from weekly_stock_openings where week_opening_date is not null )
+
+                     select 
+                     distinct week_opening_date
+                     , filing_week
+                     , week_close_price
+                     , stock_symbol
+                     , 1.00 * keyword_mentions / total_filings as keyword_percentage
+                     from stock_weekly_opening join keyword_data on stock_weekly_opening.week_opening_date = keyword_data.filing_week 
+                     where week_opening_date >= '{correlation_start_date}'
+                     and filing_type = '{filing_type}'
+                     and stock_symbol = '{stock_symbol}'
+                     order by stock_symbol, week_opening_date asc
+                     )
+
+                     select week_opening_date
+                     , stock_symbol
+                     , week_close_price
+                       , filing_week
+                     , '{keyword} Mentions' as keyword_mentions
+                     , avg(keyword_percentage) over(order by stock_symbol, week_opening_date rows 12 preceding) as keyword_mentions_rolling_avg
+                     from rolling_average_calculation
+                     order by stock_symbol, week_opening_date asc
+                     )
+
+                     select 
+                     (EXTRACT(YEAR FROM md2.filing_week) * 10000) + (EXTRACT(MONTH FROM md2.filing_week) * 100) + EXTRACT(DAY FROM md2.filing_week) as current_week
+                     , md2.keyword_mentions_rolling_avg
+                     , md2.week_close_price as current_close_price
+                     , md1.week_close_price as next_week_close_price
+                     from matched_dates as md1
+                     join matched_dates as md2 
+                     on md1.week_opening_date = md2.filing_week + interval '{interval} week'
+                     where md2.filing_week < '{end_date}'
+                     offset 3
+             '''
+    df_results = pd.read_sql(training_dataset, con=connect)
+    try:
+        X = df_results.drop(columns=['next_week_close_price'])
+        y = df_results['next_week_close_price']
+        X = X.interpolate()
+        y = y.interpolate()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=12)
+
+        # choose your model
+        model = DecisionTreeRegressor(criterion='friedman_mse', random_state=12)
+        # model = RandomForestRegressor(n_estimators=200, max_depth=20)
+        # model = LinearRegression()
+
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mae = mean_absolute_error(y_test, y_pred)
+        print('Mean absolute error:', mae)
+
+        test_dataset = f'''
+                     with matched_dates as (
+                     with rolling_average_calculation as (
+                       with keyword_data as (select * from keyword_weekly_counts where keyword = '{keyword}' and filing_type = '{filing_type}'),
+                     stock_weekly_opening as (select * from weekly_stock_openings where week_opening_date is not null )
+
+                     select 
+                     distinct week_opening_date
+                     , filing_week
+                     , week_close_price
+                     , stock_symbol
+                     , 1.00 * keyword_mentions / total_filings as keyword_percentage
+                     from stock_weekly_opening join keyword_data on stock_weekly_opening.week_opening_date = keyword_data.filing_week 
+                     where week_opening_date >= '{correlation_start_date}'
+                     and filing_type = '{filing_type}'
+                     and stock_symbol = '{stock_symbol}'
+                     order by stock_symbol, week_opening_date asc
+                     )
+
+                     select week_opening_date
+                     , stock_symbol
+                     , week_close_price
+                       , filing_week
+                     , '{keyword} Mentions' as keyword_mentions
+                     , avg(keyword_percentage) over(order by stock_symbol, week_opening_date rows 12 preceding) as keyword_mentions_rolling_avg
+                     from rolling_average_calculation
+                     order by stock_symbol, week_opening_date asc
+                     )
+
+                     select 
+                     (EXTRACT(YEAR FROM md2.filing_week) * 10000) + (EXTRACT(MONTH FROM md2.filing_week) * 100) + EXTRACT(DAY FROM md2.filing_week) as current_week
+                     , md1.week_opening_date
+                     , md2.keyword_mentions_rolling_avg
+                     , md2.week_close_price as current_close_price
+                     , md1.week_close_price as next_week_close_price
+                     from matched_dates as md1
+                     join matched_dates as md2 
+                     on md1.week_opening_date = md2.filing_week + interval '{interval} week'
+                     where md2.filing_week = '{end_date}'
+                     '''
+        df_test_full = pd.read_sql(test_dataset, con=connect)
+        df_test = df_test_full.drop(columns=['week_opening_date', 'next_week_close_price'])
+        df_test_full = df_test_full.drop_duplicates()
+        df_test = df_test.drop_duplicates()
+    except (KeyError, ValueError) as error1:
+        print(error1)
+    return df_test_full, df_test, mae, model
+
+
 # if this is returning empty dataframes, then it's because there's some new stock that entered the S&P that doesn't have
 # enough data to create a 12 week rolling average (and therefore the later queries in this function return no results)
 # to fix, run top_correlation_query_results against the DB and see what the top results are, then add that stock to the
@@ -287,7 +398,8 @@ def calculate_top_ten_forecasts(testing_timeline):
         mae_data = []
 
         for dates in datetime_list:
-            df_test_full, df_test, mae, model = train_ml_model(keyword, filing_type, stock_symbol, interval, dates)
+            df_test_full, df_test, mae, model = train_ml_model(keyword, filing_type, stock_symbol,
+                                                               interval, dates)
             full_test_data.append(df_test_full)
             mae_data.append(mae)
 
@@ -312,7 +424,8 @@ def calculate_top_ten_forecasts(testing_timeline):
         df_for_pg_upload = df_for_pg_upload.append(df_full, ignore_index=True)
     return df_for_pg_upload
 #
-# full_df_for_upload = calculate_top_ten_forecasts()
+# full_df_for_upload = calculate_top_ten_forecasts('backtest')
+# print(full_df_for_upload)
 # append_to_postgres(full_df_for_upload, 'top_five_prediction_results', 'replace')
 
 
--- precise_backtest.py
+++ precise_backtest.py
@@ -84,7 +84,7 @@ def all_time_top_correlation_query_results():
               and "Keyword" != 'cryptocurrency Mentions'
               and end_date = '{dates}'
             order by correlation desc
-            limit 50
+            limit 100
             )
 
             select distinct on (stock_symbol) *
@@ -96,8 +96,12 @@ def all_time_top_correlation_query_results():
         df_for_top_correlations = df_for_top_correlations.append(query_df, ignore_index=True)
     return df_for_top_correlations
 
-def backtesting_buy_recommendation_list():
-    df_recommended_buys = []
+
+def build_backtest_prediction_table():
+    df_for_calculating_backtest = pd.DataFrame(columns=['current_week', 'week_opening_date', 'keyword_mentions_rolling_avg',
+                                             'current_close_price', 'next_week_close_price', 'predicted_price',
+                                             'stock_symbol', 'keyword', 'start_date', 'monday_end_date',
+                                             'friday_end_date', 'time_delay', 'filing_type'])
 
     #find each end date, then use that for the backtest process
     end_dates_query = f'''
@@ -106,6 +110,7 @@ def backtesting_buy_recommendation_list():
     '''
     end_date_df = pd.read_sql(end_dates_query, con=connect)
     end_date_list = end_date_df.values.tolist()
+    #for each week in the table, find the top 10 correlation scores and return them
     for dates in end_date_list:
         dates = dates[0]
         top_correlation_query_results = f'''
@@ -125,7 +130,7 @@ def backtesting_buy_recommendation_list():
               and "Keyword" != 'cryptocurrency Mentions'
               and end_date = '{dates}'
             order by correlation desc
-            limit 50
+            limit 250
             )
 
             select distinct on (stock_symbol) *
@@ -136,110 +141,170 @@ def backtesting_buy_recommendation_list():
         query_df = pd.read_sql(top_correlation_query_results, con=connect)
         print(f'''query df: {query_df}''')
 
-        #backtest process begins here
-        #Need to figure out how to implement the end_date into this process. There is no exact end-date column. Query
-        #is currently in Mode, so check it out.
+        test_results = []
+        full_test_data = []
+
         row_range = range(0, 10)
+        #for each of the top 10 correlation scores for each week, train the ML model then predict the following weekly
+        # close (while hiding what that data is from the model)
         for rows in row_range:
             df_row = query_df.iloc[rows]
             stock_symbol = df_row['stock_symbol']
             keyword = df_row['keyword']
             correlation_start_date = df_row['start_date']
+            monday_end_date = df_row['end_date']
             interval = df_row['time_delay']
             filing_type = df_row['filing_type']
 
-            buy_recommendation = f'''with prediction_testing as (
-              with prices as (
-              select stock_symbol, start_date, keyword, time_delay, filing_type, to_date(cast(current_week as text), 'YYYYMMDD') as current_week, current_close_price
-              , week_opening_date as prediction_date, predicted_price as next_week_predicted_close, next_week_close_price
-              , case when next_week_close_price > current_close_price then 'price increased'
-                  when next_week_close_price < current_close_price then 'price decreased'
-                  when next_week_close_price is null then 'no price comparison'
-                  else 'price the same'
-                  end as actual_price_movement
-              , next_week_close_price - current_close_price as actual_price_change
-              , (next_week_close_price / current_close_price) - 1 as actual_price_change_percentage
-              , case when current_close_price < predicted_price then 'price increased'
-                  when current_close_price > predicted_price then 'price decreased'
-                  when predicted_price is null then 'no price comparison'
-                  else 'price the same'
-                  end as predicted_price_movement
-              , predicted_price - current_close_price as predicted_price_change 
-              , 1 - (current_close_price / predicted_price) as predicted_price_change_percentage 
-              from top_five_prediction_results
-              where 
-              stock_symbol = '{stock_symbol}'
-              and keyword = '{keyword}'
-              and start_date = '{correlation_start_date}'
-              and time_delay = '{interval}'
-              and filing_type = '{filing_type}'
-              order by current_week asc
-              )
-    
-              SELECT
-              stock_symbol, keyword, time_delay, filing_type, start_date, current_week, prediction_date, current_close_price
-              , next_week_close_price  as actual_price_week_of_prediction, next_week_predicted_close as predicted_close_price
-              , actual_price_change
-              , predicted_price_change
-              , predicted_price_change_percentage
-              , case when actual_price_movement = 'no price comparison' then 'no price comparison'
-                  when actual_price_movement = predicted_price_movement then 'correct prediction'
-                  else 'incorrect prediction'
-                  end as prediction_validation
-              , actual_price_change - predicted_price_change as actual_prediction_delta
-              , case when actual_price_change = 0 then null else (actual_price_change - predicted_price_change) / current_close_price end as actual_prediction_percentage_delta
-              from prices
-            ),
-    
-            next_week_close as (
-              select stock_symbol
-              , week_open_price
-              , week_close_price
-                , week_opening_date
-                , Lead(week_open_price) OVER (
-              ORDER BY stock_symbol, week_opening_date) as one_week_later_open_price
-              , Lead(week_close_price) OVER (
-                ORDER BY stock_symbol, week_opening_date) as one_week_later_close_price
-              from weekly_stock_openings 
-              where week_opening_date is not null 
-            )
-    
-            select  
-            prediction_testing.stock_symbol
-            , current_week as buy_week
-            --need to change this to the weekly open!
-            , next_week_close.one_week_later_open_price as buy_price
-            , next_week_close.one_week_later_close_price as cashout_price
-            , prediction_date
-            , prediction_testing.predicted_close_price
-            , predicted_price_change
-            , predicted_price_change_percentage
-            , prediction_testing.keyword
-            , prediction_testing.time_delay
-            , prediction_testing.filing_type
-            , prediction_testing.start_date
-    
-            from prediction_testing
-            join next_week_close on prediction_testing.current_week = next_week_close.week_opening_date
-            and prediction_testing.stock_symbol = next_week_close.stock_symbol
-            where next_week_close.week_opening_date >= '2022-07-01'
-            and predicted_close_price > current_close_price
-            --pulling the data necessarily to say buy in a given week
-                '''
-            buy_df_results = pd.read_sql(buy_recommendation, con=connect)
-            if buy_df_results.empty:
-                continue
+            #adjusts start date and end dates to the real start & end dates as pulled from the DB
+            correlation_start_date = datetime.strptime(correlation_start_date, '%Y-%m-%d').date()
+            correlation_start_date = correlation_start_date + timedelta((0 - correlation_start_date.weekday()) % 7)
+            monday_end_date = datetime.strptime(monday_end_date, '%Y-%m-%d').date()
+            rounded_friday = monday_end_date + timedelta(days=(4 - monday_end_date.weekday()))
+            if rounded_friday == monday_end_date:
+                rounded_friday = rounded_friday
             else:
-                buy_df_results['predicted_price_change_percentage'] = buy_df_results[
-                    'predicted_price_change_percentage'].apply(format_percent)
-                df_recommended_buys.append(buy_df_results)
+                rounded_friday = rounded_friday - timedelta(days=7)
+            friday_end_date = rounded_friday
+
+            df_test_full, df_test, mae, model = forecast_top_stocks_model.train_narrow_ml_model(keyword, filing_type,
+                                                                                         stock_symbol, interval,
+                                                                                         monday_end_date, correlation_start_date)
+
+            full_test_data.append(df_test_full)
+            try:
+                prediction = model.predict(df_test)
+                test_results.append(prediction)
+                print('Predicted stock price:', prediction)
+            except (KeyError, ValueError) as error:
+                print(error)
+                continue
+
+        #build the table with all of the predictions
+        df_test = pd.DataFrame(test_results, columns=['predicted_price'])
+        full_results_df = pd.concat(full_test_data, ignore_index=True)
+        df_full = pd.DataFrame(full_results_df)
+        df_full['predicted_price'] = df_test
+        df_full['stock_symbol'] = stock_symbol
+        df_full['keyword'] = keyword
+        df_full['start_date'] = correlation_start_date
+        df_full['friday_end_date'] = friday_end_date
+        df_full['monday_end_date'] = monday_end_date
+        df_full['time_delay'] = interval
+        df_full['filing_type'] = filing_type
+        df_full = df_full.drop_duplicates()
+        df_for_calculating_backtest = df_for_calculating_backtest.append(df_full, ignore_index=True)
+    return df_for_calculating_backtest
+
+df_for_calculating_backtest = build_backtest_prediction_table()
+forecast_top_stocks_model.append_to_postgres(df_for_calculating_backtest, 'precise_backtest_top_predictions', 'replace')
+
+def backtesting_buy_recommendation_list():
+    df_recommended_buys = []
+    query_df = forecast_top_stocks_model.top_correlation_query_results()
+
+    row_range = range(0, 10)
+    for rows in row_range:
+        df_row = query_df.iloc[rows]
+        stock_symbol = df_row['stock_symbol']
+        keyword = df_row['keyword']
+        correlation_start_date = df_row['start_date']
+        interval = df_row['time_delay']
+        filing_type = df_row['filing_type']
+
+        buy_recommendation = f'''with prediction_testing as (
+          with prices as (
+          select stock_symbol, start_date, keyword, time_delay, filing_type, to_date(cast(current_week as text), 'YYYYMMDD') as current_week, current_close_price
+          , week_opening_date as prediction_date, predicted_price as next_week_predicted_close, next_week_close_price
+          , case when next_week_close_price > current_close_price then 'price increased'
+              when next_week_close_price < current_close_price then 'price decreased'
+              when next_week_close_price is null then 'no price comparison'
+              else 'price the same'
+              end as actual_price_movement
+          , next_week_close_price - current_close_price as actual_price_change
+          , (next_week_close_price / current_close_price) - 1 as actual_price_change_percentage
+          , case when current_close_price < predicted_price then 'price increased'
+              when current_close_price > predicted_price then 'price decreased'
+              when predicted_price is null then 'no price comparison'
+              else 'price the same'
+              end as predicted_price_movement
+          , predicted_price - current_close_price as predicted_price_change 
+          , 1 - (current_close_price / predicted_price) as predicted_price_change_percentage 
+          from precise_backtest_top_predictions
+          where 
+          stock_symbol = '{stock_symbol}'
+          and keyword = '{keyword}'
+          and start_date = '{correlation_start_date}'
+          and time_delay = '{interval}'
+          and filing_type = '{filing_type}'
+          order by current_week asc
+          )
+
+          SELECT
+          stock_symbol, keyword, time_delay, filing_type, start_date, current_week, prediction_date, current_close_price
+          , next_week_close_price  as actual_price_week_of_prediction, next_week_predicted_close as predicted_close_price
+          , actual_price_change
+          , predicted_price_change
+          , predicted_price_change_percentage
+          , case when actual_price_movement = 'no price comparison' then 'no price comparison'
+              when actual_price_movement = predicted_price_movement then 'correct prediction'
+              else 'incorrect prediction'
+              end as prediction_validation
+          , actual_price_change - predicted_price_change as actual_prediction_delta
+          , case when actual_price_change = 0 then null else (actual_price_change - predicted_price_change) / current_close_price end as actual_prediction_percentage_delta
+          from prices
+        ),
+
+        next_week_close as (
+          select stock_symbol
+          , week_open_price
+          , week_close_price
+            , week_opening_date
+            , Lead(week_open_price) OVER (
+          ORDER BY stock_symbol, week_opening_date) as one_week_later_open_price
+          , Lead(week_close_price) OVER (
+            ORDER BY stock_symbol, week_opening_date) as one_week_later_close_price
+          from weekly_stock_openings 
+          where week_opening_date is not null 
+        )
+
+        select  
+        prediction_testing.stock_symbol
+        , current_week as buy_week
+        --need to change this to the weekly open!
+        , next_week_close.one_week_later_open_price as buy_price
+        , next_week_close.one_week_later_close_price as cashout_price
+        , prediction_date
+        , prediction_testing.predicted_close_price
+        , predicted_price_change
+        , predicted_price_change_percentage
+        , prediction_testing.keyword
+        , prediction_testing.time_delay
+        , prediction_testing.filing_type
+        , prediction_testing.start_date
+
+        from prediction_testing
+        join next_week_close on prediction_testing.current_week = next_week_close.week_opening_date
+        and prediction_testing.stock_symbol = next_week_close.stock_symbol
+        where next_week_close.week_opening_date >= '2022-07-01'
+        and predicted_close_price > current_close_price
+            '''
+        buy_df_results = pd.read_sql(buy_recommendation, con=connect)
+        if buy_df_results.empty:
+            continue
+        else:
+            buy_df_results['predicted_price_change_percentage'] = buy_df_results[
+                'predicted_price_change_percentage'].apply(format_percent)
+            df_recommended_buys.append(buy_df_results)
     df_for_pg_upload = pd.concat(df_recommended_buys, ignore_index=True)
     df_for_pg_upload = df_for_pg_upload.sort_values(by=['buy_week', 'stock_symbol'])
     print(df_for_pg_upload)
-    # append_to_postgres(df_for_pg_upload, 'backtest_buys_test', 'replace')
+    forecast_top_stocks_model.append_to_postgres(df_for_pg_upload, 'precise_backtest_buys_test', 'replace')
     return df_for_pg_upload
 
-# backtesting_buy_recommendation_list()
+
+# df_for_pg_upload = backtesting_buy_recommendation_list()
+# print(df_for_pg_upload)
 
 def calculate_returns():
     cash_in_hand = 1000
@@ -313,3 +378,4 @@ def comparing_returns_vs_sandp():
     df_for_chart = pd.merge(df_for_join, returns_df, how='inner', on='week_of_purchases')
 
     return df_for_chart
+
